cae7b9c6875190c0be545adc16e96ebfd75fdac4
Onseted <108707011+Onseted@users.noreply.github.com>
2022-08-22 15:36:53 +0900

    ""

--- meomeok/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-# from django.contrib.auth.models import User
-from .models import Restaurant
-
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = '__all__'
-
